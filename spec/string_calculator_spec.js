3b98d3313b4908e2c59640ed2fc62caacb425f85
KatzeWien <daniela@sander.or.at>
2024-06-01 14:14:49 +0200

    Nächsten Testcases wurden geschrieben von Daniela

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -12,4 +12,10 @@ describe('DiceGame',function(){
       it('return 2 for [6,6,4,4,1,4]',function(){
         expect(test.add([6,6,4,4,1,3])).toEqual(2);
       });
+      it('return to low if array less than 4 dice',function(){
+        expect(test.add([1,2,3])).toEqual(-1);
+      });
+      it('return to high if array higher than 6 dice',function(){
+        expect(test.add([1,2,3,4,5,6,1])).toEqual(-1);
+      });
 });
