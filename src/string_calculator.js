2cb520dc68797aa146bdb44a0f675304c16091f8
KatzeWien <daniela@sander.or.at>
2024-06-01 14:29:31 +0200

    Nächsten Testfälle werden abgedeckt von Daniela

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -4,4 +4,11 @@ DiceGame.prototype.add=function(array)
       if(array.length==0){return -1;}
       if(array.length<4||array.length>6)
         {return -1;}
+      for(let i = 0; i < array.length; i++)
+        {
+          if(array[i] > 6 || array[i] < 1)
+            {
+              {return -1;}
+            }
+        }
 }
